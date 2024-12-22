49e4ac65019a06468ed0033c3874075fc73e6f63
Kalyan Reddy Bejjanki <kalyanreddybejjanki@Kalyans-MacBook-Air.local>
2024-12-22 16:44:13 +0530

    minor fixes

--- service/customer.js
+++ service/customer.js
@@ -125,7 +125,7 @@ const login = async (req, res) => {
       res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "Strict",
+        sameSite: "None",
         maxAge: 3600000,
       });
 
