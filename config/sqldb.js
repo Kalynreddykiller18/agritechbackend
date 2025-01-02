dd4faf8893a2af312c65dc7a8112f601d0c8a5a5
Kalyan Reddy Bejjanki <kalyanreddybejjanki@Kalyans-MacBook-Air.local>
2025-01-02 17:03:56 +0530

    SQl connection pool created

--- config/sqldb.js
+++ config/sqldb.js
@@ -1,11 +1,15 @@
 import mysql from "mysql2/promise";
 
+// creating connection pool
 const connection = await mysql.createConnection({
   host: process.env.SQL_HOST,
   user: process.env.SQL_USER,
   password: process.env.SQL_PASS,
   database: process.env.SQL_DB,
   timezone: "+05:30",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
 });
 
 export default connection;
